22e8f94baa8d14504d052f947ff6ab16796f860d
Retro Techie <retrotechie@proton.me>
2023-01-29 20:09:03 -0800

    feat: perform `Save As` action
    Issue #2

--- Notepad/mainwindow.cpp
+++ Notepad/mainwindow.cpp
@@ -30,23 +30,49 @@ void MainWindow::on_actionNew_triggered()
 /*!
  * \brief MainWindow::on_actionOpen_triggered
  *
- * Open and read file.
+ * Open and read a file.
  */
 void MainWindow::on_actionOpen_triggered()
 {
     QString fileName = QFileDialog::getOpenFileName(this, "Open file");
     QFile file(fileName);
-    currentFile = fileName;
 
     if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
         QMessageBox::warning(this, "Warning", "Cannot open file " + file.errorString());
+        return;
     }
+
+    currentFile = fileName;
 
     setWindowTitle(fileName);
 
     QTextStream in(&file);  // reading text
     QString text = in.readAll();
     ui->textEdit->setText(text);
+    file.close();
+}
+
+/*!
+ * \brief MainWindow::on_actionSave_As_triggered
+ *
+ * Save current text to a file.
+ */
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(fileName);
+
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file " + file.errorString());
+    }
+
+    currentFile = fileName;
+
+    setWindowTitle(fileName);
+
+    QTextStream out(&file); // writing text
+    QString text = ui->textEdit->toPlainText();
+    out << text;
     file.close();
 }
 
--- Notepad/mainwindow.h
+++ Notepad/mainwindow.h
@@ -27,6 +27,8 @@ private slots:
 
     void on_actionOpen_triggered();
 
+    void on_actionSave_As_triggered();
+
 private:
     Ui::MainWindow *ui;
 
